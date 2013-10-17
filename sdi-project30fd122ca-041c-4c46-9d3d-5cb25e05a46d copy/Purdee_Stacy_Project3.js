3b7ca8ab841b9547b70de07a43a0c75b0cb101f1
Stacy Purdee <stacy.purdee@gmail.com>
2013-10-17 13:56:02 -0400

    JSON Data

--- sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d copy/Purdee_Stacy_Project3.js
+++ sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d copy/Purdee_Stacy_Project3.js
@@ -15,3 +15,27 @@ var myChoresAre = ["wash dishes", "vacuum", " and dust"];
 var myFavBooks  = [" Beginning JavaScript ", " JavaScript for Absolute Beginners ", " and Step-by-Step JavaScript "];
 var minsPerBook = [45, 20, 30];
 var doChores    = true;
+
+console.log("My name is " + myName + " and it is " + doChores+" that I have " + myChores + " chores to do before lunch.");
+
+console.log("My chores are " + myChoresAre + " .");
+
+//JSON Data
+var iRead = {
+    "myBooks": {
+        "book1":{ 
+            "name": "Beginning JavaScript",
+            "version": "Electronic"
+        },
+        "book2":{
+            "name": "Javascript for Absolute Beginners",
+            "version": "Hardback" 
+        },
+        "book3": {
+            "name": "Step by Step JavScript",
+            "version": "Electronic"
+        }
+        
+        }
+   
+    };
